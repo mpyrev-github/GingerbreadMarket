4b94d431e757a2cb3ddef7acf6a0532745811f5c
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-08-17 22:45:04 +0500

    Add Context to Database connection

---
+++ GingerbreadMarket/Models/OrdersContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GingerbreadMarket.Models
+{
+    public class OrdersContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=127.0.0.1;Database=OrdersDb;Username=webuser;Password=webuser");
+    }
+}
