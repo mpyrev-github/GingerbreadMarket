951b259d628ec2e4528e9ba6bb00118046e25f57
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-08-17 18:40:06 +0500

    Fix some bugs

---
+++ GingerbreadMarket/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GingerbreadMarket.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+        public string Email { get; set; }
+        public bool IsSell { get; set; }
+    }
+}
