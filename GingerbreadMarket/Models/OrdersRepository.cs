e6869b2e7b9b2910459085dba9dbf63dd20ca791
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-08-19 05:19:52 +0500

    Refactoring code.

--- GingerbreadMarket/Models/OrdersRepository.cs
+++ GingerbreadMarket/Models/OrdersRepository.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace GingerbreadMarket.Models
 {
@@ -49,66 +47,68 @@ namespace GingerbreadMarket.Models
         private void AddDeal(OrdersContext db, Order item)
         {
             Deal deal = new Deal();
+
             if (db.Deals.Count() != 0)
                 deal.Id = db.Deals.OrderBy(o => o.Id).Last().Id + 1;
             else deal.Id = 1;
+
             deal.DealDate = DateTime.Now;
+
             if (item.IsSell)
+            {
+                var BuyDbItem = db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First();
                 deal.SellDate = DateTime.Now;
-            else deal.SellDate = db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Date;
-            if (item.IsSell)
-                deal.BuyDate = db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Date;
-            else deal.BuyDate = DateTime.Now;
-            if (item.IsSell)
-                deal.Price = db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Price;
-            else deal.Price = db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Price;
-            if (item.IsSell)
-                deal.BuyEmail = db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Email;
-            else deal.BuyEmail = item.Email;
-            if (item.IsSell)
+                deal.BuyDate = BuyDbItem.Date;
+                deal.Price = BuyDbItem.Price;
+                deal.BuyEmail = BuyDbItem.Email;
                 deal.SellEmail = item.Email;
-            else deal.SellEmail = db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Email;
-            if (UpdateCount(db, item))
-            {
-                deal.Count = item.Count;
-                item.Count = 0;
-            }
-            else if (item.IsSell)
-            {
-                deal.Count = db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Count;
-                Remove(db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Id);
+
+                if (UpdateDbCount(BuyDbItem, item))
+                {
+                    deal.Count = item.Count;
+                    item.Count = 0;
+                }
+                else
+                {
+                    deal.Count = BuyDbItem.Count;
+                    Remove(BuyDbItem.Id);
+                }
             }
             else
             {
-                deal.Count = db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Count;
-                Remove(db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Id);
+                var SellDbItem = db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First();
+                deal.SellDate = SellDbItem.Date;
+                deal.BuyDate = DateTime.Now;
+                deal.Price = SellDbItem.Price;
+                deal.BuyEmail = item.Email;
+                deal.SellEmail = SellDbItem.Email;
+
+                if (UpdateDbCount(SellDbItem, item))
+                {
+                    deal.Count = item.Count;
+                    item.Count = 0;
+                }
+                else
+                {
+                    deal.Count = SellDbItem.Count;
+                    Remove(SellDbItem.Id);
+                }
             }
             db.Deals.Add(deal);
             db.SaveChanges();
         }
-        private bool UpdateCount(OrdersContext db, Order item)
+        private bool UpdateDbCount(Order DbOrder, Order item)
         {
-            if (item.IsSell)
-                if (db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Count > item.Count)
-                {
-                    db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Count -= item.Count;
-                    return true;
-                }
-                else
-                {
-                    item.Count -= db.Orders.Where(o => o.IsSell == false).OrderBy(o => o.Price).First().Count;
-                    return false;
-                }
-            else if (db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Count > item.Count)
-                {
-                    db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Count -= item.Count;
-                    return true;
-                }
-                else
-                {
-                    item.Count -= db.Orders.Where(o => o.IsSell == true).OrderByDescending(o => o.Price).First().Count;
-                    return false;
-                }
+            if (DbOrder.Count > item.Count)
+            {
+                DbOrder.Count -= item.Count;
+                return true;
+            }
+            else
+            {
+                item.Count -= DbOrder.Count;
+                return false;
+            }
         }
         private void Remove(int id)
         {
@@ -122,25 +122,9 @@ namespace GingerbreadMarket.Models
         public void RemoveDeals()
         {
             foreach (var deal in db.Deals)
-            db.Deals.Remove(deal);
-            db.SaveChanges();
-        }
+                db.Deals.Remove(deal);
 
-        public bool Update(Order item)
-        {
-            Order storedItem = GetOrder(item.Id);
-            if (storedItem != null)
-            {
-                storedItem.Price = item.Price;
-                storedItem.Count = item.Count;
-                storedItem.Email = item.Email;
-                storedItem.IsSell = item.IsSell;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            db.SaveChanges();
         }
     }
 }
